b43214ff06175f1ded3d8512598c68b005291fa9
BZSY55 <2476203449@qq.com>
2024-01-29 16:19:23 +0800

    change gpt/map.py

--- gpt-api/map.py
+++ gpt-api/map.py
@@ -6,20 +6,16 @@ from sklearn.metrics import average_precision_score
 base_path = "/data/caokun/huge_model/chatGLM/ChatGLM-6B-main/ptuning/output/test/finetune/"
 num = 5
 dic_list = ["result/gpt-3.5-turbo2.json", "result/gpt-4.json"]
-with open("ID_num_dic.json") as read_file:
-    trans_dic = json.load(read_file)
 for i in range(len(dic_list)):
     result_list = []
-    result_dic = {}
     with open(dic_list[i], "r") as read_file:
-        all_lines = read_file.readlines()
-    for item in trans_dic.keys():
-        this_list = trans_dic[item]
+        result_dic = json.load(read_file)
+    for item in result_dic.keys():
+        this_list = result_dic[item]
         pre = []
         res = []
         for jtem in this_list:
-            data = json.loads(all_lines[jtem].strip())
-            if data["labels"] == "Yes":
+            if jtem["labels"] == "Yes":
                 res.append(1)
             else:
                 res.append(0)
@@ -27,11 +23,11 @@ for i in range(len(dic_list)):
             #    pre.append(1)
             #else:
             #    pre.append(0)
-            if "yes" in data["predict"] or "Yes" in data["predict"]:
+            if "yes" in jtem["predict"] or "Yes" in jtem["predict"]:
                 pre.append(1)
-            elif "not important" in data["predict"]:
+            elif "not important" in jtem["predict"]:
                 pre.append(0)
-            elif "important" in data["predict"]:
+            elif "important" in jtem["predict"]:
                 pre.append(1)
             else:
                 pre.append(0)
